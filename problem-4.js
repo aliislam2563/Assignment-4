3d7cc193839269a45dec3704d8a0330abbc99078
aliislam2563 <aliislam2563@gamil.com>
2023-08-10 20:38:14 +0600

    problem 4 solved

--- problem-4.js
+++ problem-4.js
@@ -1,1 +1,19 @@
-function findAddress(object)
+function findAddress(object){
+    if(typeof obj != "object"){
+        return "invalid";
+    }
+    else{
+        let street = obj.street || "_";
+        let house = obj.house || "_";
+        let society = obj.society || "_";
+        let result =  console.log(street, house, society);
+        return result;
+    }
+}
+let obj = {
+    street: 10,
+    house: "15A",
+    society: "Earth Perfect"
+}
+let output = findAddress(obj);
+return output;
